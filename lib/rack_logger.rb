c77ef0bb49ace7e820e9c6ce55c8f776ad10d7e2
Allen Wei <digruby@gmail.com>
2014-05-05 13:43:52 +0800

    support activesuport notification

--- lib/rack_logger.rb
+++ lib/rack_logger.rb
@@ -1,5 +1,5 @@
 require "rack_logger/version"
-
+require "uuidtools"
 require 'cgi'
 # Completed 404 Not Found in 105ms (Views: 4.6ms | ActiveRecord: 2.3ms | Sphinx: 0.0ms | Gateway: 86.3ms)
 class RackLogger
@@ -25,15 +25,18 @@ class RackLogger
     begin
       request = Rack::Request.new(env)
       time_start = Time.now
-      logger.info "Started #{request.request_method} \"#{CGI.unescape(request.url)}\" for #{request.ip} at #{Time.now} #{request.user_agent}"
+      logger.info "Started #{request.request_method} \"#{request.url}\" for #{request.ip} at #{Time.now} #{request.user_agent}"
+      Thread.current[:request_id] ||= UUIDTools::UUID.random_create.to_s
       status, header, body = @app.call(env)
       # million seconds
       time_cost = (Time.now - time_start) * 1000
       header["x-runtime"] = "%.2fms" % time_cost
+      header["x-request-id"] = Thread.current[:request_id]
 
       [status, header, body]
     ensure
       benchmarks = []
+      payload = {}
 
       time_cost ||= (Time.now - time_start) * 1000
 
@@ -43,16 +46,30 @@ class RackLogger
 
         if t = subscriber_class.send(:runtime)
           benchmarks << "#{label}: %.2fms" % t
+          payload["#{label.downcase}_runtime"] = t
           subscriber_class.send(:reset_runtime)
         end
       end
 
-      msg = "Completed #{request.request_method} \"#{CGI.unescape(request.url)}\" #{status}"
+      msg = "Completed #{request.request_method} #{ CGI.unescape(request.url)} #{status}"
       msg << " in %.2fms" % time_cost
       msg << " ("
       msg << benchmarks.join(' | ')
       msg << ") "
+
+      payload[:method] = request.request_method
+      payload[:url] = CGI.unescape(request.url)
+      uri = URI(request.url)
+      payload[:host] = uri.host
+      payload[:path] = uri.path
+      payload[:query] = uri.query
+
+      payload[:status_code] = status
+      payload[:duration] = time_cost
+      payload[:header] = header
+      payload[:request_id] = Thread.current[:request_id]
       if status >= 400
+        payload[:body] = body
         body_str = ""
         if body.respond_to? :each
           body.each do |b|
@@ -67,6 +84,9 @@ class RackLogger
       else
         msg << "\n\n"
         logger.info msg
+      end
+      if defined? ActiveSupport
+        ActiveSupport::Notifications.instrument "process.rack_logger", payload
       end
     end
   end
--- rack_logger.gemspec
+++ rack_logger.gemspec
@@ -17,6 +17,8 @@ Gem::Specification.new do |gem|
 
 
   gem.add_runtime_dependency 'rack'
+  gem.add_runtime_dependency 'uuidtools'
 
   gem.add_development_dependency 'rake'
+  gem.add_development_dependency 'activesupport'
 end
